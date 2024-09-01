73938480c46e5e880ab27b450b7d2d4871b89025
tappoy <tappoy1234@gmail.com>
2024-09-01 13:58:46 +0900

    chore: change buf -o path

--- proto/_buf/go/file/v1/filev1connect/private.connect.go
+++ proto/_buf/go/file/v1/filev1connect/private.connect.go
@@ -8,7 +8,7 @@ import (
 	connect "connectrpc.com/connect"
 	context "context"
 	errors "errors"
-	v1 "github.com/tappoy/jns/proto/gen/file/v1"
+	v1 "github.com/tappoy/jns/proto/_buf/go/file/v1"
 	http "net/http"
 	strings "strings"
 )
